d5281ed538042dc7e342885d8ba27c38dd89683e
Globakor <yunglady423@gmail.com>
2024-11-10 23:35:31 +0000

    assignment to create array of objects

---
+++ javascript/exercise.js
@@ -0,0 +1,32 @@
+ let students = [ 
+      {Name: "Juliet Asamoah", Age:21, Grade: "A" },
+    {Name: "Abigail Yendoh", Age:22, Grade: "C" },
+   {Name: "Rasheeda Mohammed", Age:23, Grade: "B" },
+   {Name: "Albert Akoto Mensah", Age:20, Grade:"B" },
+  ];
+  
+     console.log(students);c
+
+ // Adding students to the array
+
+ let student1 =  {Name: "Alice Nyarko", Age:16, Grade: "A" } ;
+ let student2 =  { Name: "Bob Edward", Age:23, Grade: "B" } ;
+ let student3 =  {Name: "Charlie Arthur", Age:17, Grade: "C" } ;
+
+ students.push(student1);
+ students.push(student2);
+ students.push(student3);
+
+ console.log(students);
+
+  
+console.log(students);
+
+ // Function to display all students' information
+students.forEach((student) => { 
+   console.log(students);
+} )
+
+
+
+     
